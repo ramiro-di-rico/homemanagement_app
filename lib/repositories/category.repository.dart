ea8de02798f2a950f3da911bbf9b25f7f0b747f9
Ramiro Di Rico <rdirico@makingsense.com>
2021-07-11 22:10:20 -0300

    sort categories by name

--- lib/repositories/category.repository.dart
+++ lib/repositories/category.repository.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:home_management_app/models/category.dart';
 import 'package:home_management_app/services/category.service.dart';
 
-class CategoryRepository extends ChangeNotifier{
+class CategoryRepository extends ChangeNotifier {
   CategoryService categoryService;
   final List<CategoryModel> categories = [];
   final List<CategoryModel> activeCategories = [];
@@ -12,7 +12,10 @@ class CategoryRepository extends ChangeNotifier{
   Future load() async {
     var result = await categoryService.fetch();
     categories.addAll(result);
-    activeCategories.addAll(categories.where((element) => element.isActive).toList());
+    activeCategories
+        .addAll(categories.where((element) => element.isActive).toList());
+    categories.sort((a, b) => a.name.compareTo(b.name));
+    activeCategories.sort((a, b) => a.name.compareTo(b.name));
     notifyListeners();
   }
 }
