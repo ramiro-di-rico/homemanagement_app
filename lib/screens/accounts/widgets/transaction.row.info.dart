f2e7ccd28603a1f07da7779effd501f1bc40bcf5
Ramiro Di Rico <ramiro.di.rico@gmail.com>
2022-05-22 21:14:28 -0300

    add slidable package and actions for transactions list

--- lib/screens/accounts/widgets/transaction.row.info.dart
+++ lib/screens/accounts/widgets/transaction.row.info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:get_it/get_it.dart';
 import 'package:home_management_app/models/category.dart';
 import 'package:home_management_app/models/transaction.dart';
@@ -20,71 +21,85 @@ class TransactionRowInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Dismissible(
-      key: Key(transaction.id.toString()),
-      direction: DismissDirection.endToStart,
-      background: Container(
-        color: Colors.blueAccent,
-      ),
-      secondaryBackground: Container(
-        color: Colors.redAccent,
-      ),
-      onDismissed: (direction) => remove(transaction, index),
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
-        child: Padding(
-          padding: EdgeInsets.all(5),
-          child: ListTile(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => EditTransactionScreen(transaction),
-                ),
-              );
-            },
-            contentPadding: EdgeInsets.symmetric(horizontal: 10),
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(transaction.name),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
-                      child: Text(
-                        transaction.price % 1 == 0
-                            ? transaction.price.toStringAsFixed(0)
-                            : transaction.price.toStringAsFixed(2),
-                        style: TextStyle(
-                            color: transaction.transactionType ==
-                                    TransactionType.Income
-                                ? Colors.greenAccent
-                                : Colors.redAccent),
+    return Slidable(
+        endActionPane: ActionPane(
+          motion: ScrollMotion(),
+          children: [
+            SlidableAction(
+              autoClose: true,
+              onPressed: (context) {
+                var copy = transaction.duplicate();
+                transactionRepository.add(copy);
+              },
+              icon: Icons.copy,
+              backgroundColor: Colors.lightBlueAccent,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            SlidableAction(
+              autoClose: true,
+              icon: Icons.delete,
+              backgroundColor: Colors.redAccent,
+              borderRadius: BorderRadius.circular(20),
+              onPressed: (context) => remove(transaction, index),
+            ),
+          ],
+        ),
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+          child: Padding(
+            padding: EdgeInsets.all(5),
+            child: ListTile(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => EditTransactionScreen(transaction),
+                  ),
+                );
+              },
+              contentPadding: EdgeInsets.symmetric(horizontal: 10),
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(transaction.name),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                        child: Text(
+                          transaction.price % 1 == 0
+                              ? transaction.price.toStringAsFixed(0)
+                              : transaction.price.toStringAsFixed(2),
+                          style: TextStyle(
+                              color: transaction.transactionType ==
+                                      TransactionType.Income
+                                  ? Colors.greenAccent
+                                  : Colors.redAccent),
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        DateFormat.MMMd().format(transaction.date),
                       ),
-                    )
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text(
-                      DateFormat.MMMd().format(transaction.date),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 15),
-                      child: Chip(
-                        label: Text(category.name),
-                      ),
-                    )
-                  ],
-                )
-              ],
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: Chip(
+                          label: Text(category.name),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
-      ),
-    );
+        closeOnScroll: true);
   }
 
   Future remove(item, index) async {
