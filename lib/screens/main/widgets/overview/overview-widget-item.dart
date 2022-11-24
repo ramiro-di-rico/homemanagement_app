e49003916e98b301fc53cd14ec2cfc3e9aca6f87
Ramiro <ramiro.di.rico@gmail.com>
2022-11-24 16:52:04 -0300

    add indicators

--- lib/screens/main/widgets/overview/overview-widget-item.dart
+++ lib/screens/main/widgets/overview/overview-widget-item.dart
@@ -35,8 +35,7 @@ class _OverviewWidgetState extends State<OverviewWidget> {
           child: Padding(
         padding: const EdgeInsets.all(5),
         child: Column(children: [
-          ListTile(
-              leading: Icon(Icons.donut_large), title: Text('Transactions')),
+          ListTile(leading: Icon(Icons.donut_large), title: Text('Overall')),
           Expanded(
             child: Row(children: [
               Expanded(
@@ -46,20 +45,21 @@ class _OverviewWidgetState extends State<OverviewWidget> {
                         child: Skeleton(
                       isLoading: overall == null,
                       skeleton: SkeletonAvatar(),
-                      child: PieChart(PieChartData(sections: [
-                        PieChartSectionData(
-                            color: Colors.orange[200],
-                            radius: 30,
-                            title: overall?.incomeTransactions.toString(),
-                            value: overall?.incomeTransactions?.toDouble()),
-                        PieChartSectionData(
-                            color: Colors.teal[200],
-                            radius: 30,
-                            title: overall?.outcomeTransactions.toString(),
-                            value: overall?.outcomeTransactions?.toDouble()),
-                      ])),
+                      child: PieChart(PieChartData(
+                        sections: [
+                          PieChartSectionData(
+                              color: Colors.orange[200],
+                              radius: 30,
+                              title: overall?.incomeTransactions.toString(),
+                              value: overall?.incomeTransactions?.toDouble()),
+                          PieChartSectionData(
+                              color: Colors.teal[200],
+                              radius: 30,
+                              title: overall?.outcomeTransactions.toString(),
+                              value: overall?.outcomeTransactions?.toDouble()),
+                        ],
+                      )),
                     )),
-                    Text('Count'),
                   ],
                 ),
               ),
@@ -75,20 +75,19 @@ class _OverviewWidgetState extends State<OverviewWidget> {
                             skeleton: SkeletonAvatar(),
                             child: PieChart(PieChartData(sections: [
                               PieChartSectionData(
-                                  color: Colors.deepPurple[200],
+                                  color: Colors.orange[200],
                                   radius: 30,
                                   title: shortenBigNumber
                                       .shortNumber(overall?.totalIncome),
                                   value: overall?.totalIncome?.toDouble()),
                               PieChartSectionData(
-                                  color: Colors.amber[200],
+                                  color: Colors.teal[200],
                                   radius: 30,
                                   title: shortenBigNumber
                                       .shortNumber(overall?.totalOutcome),
                                   value: overall?.totalOutcome?.toDouble()),
                             ])),
                           )),
-                          Text('Amount')
                         ],
                       ),
                     ),
@@ -96,6 +95,25 @@ class _OverviewWidgetState extends State<OverviewWidget> {
                 ),
               )
             ]),
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Indicator(
+                color: Colors.orange[200],
+                text: 'Income',
+                isSquare: true,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Indicator(
+                color: Colors.teal[200],
+                text: 'Outcome',
+                isSquare: true,
+              ),
+            ],
           )
         ]),
       )),
@@ -107,5 +125,47 @@ class _OverviewWidgetState extends State<OverviewWidget> {
     setState(() {
       overall = fetchedOverall;
     });
+  }
+}
+
+class Indicator extends StatelessWidget {
+  const Indicator({
+    this.color,
+    this.text,
+    this.isSquare,
+    this.size = 16,
+    this.textColor = const Color(0xff505050),
+  });
+  final Color color;
+  final String text;
+  final bool isSquare;
+  final double size;
+  final Color textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: size,
+          height: size,
+          decoration: BoxDecoration(
+            shape: isSquare ? BoxShape.rectangle : BoxShape.circle,
+            color: color,
+          ),
+        ),
+        const SizedBox(
+          width: 4,
+        ),
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: textColor,
+          ),
+        )
+      ],
+    );
   }
 }
