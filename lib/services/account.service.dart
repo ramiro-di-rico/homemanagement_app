dff95bb42265088d38069856bc07f987e08767de
Ramiro Di Rico <ramiro.di.rico@pm.me>
2023-06-23 15:01:21 -0300

    Use v3 endpoints

--- lib/services/account.service.dart
+++ lib/services/account.service.dart
@@ -6,29 +6,30 @@ import 'dart:convert';
 class AccountService {
   AuthenticationService authenticationService;
   ApiServiceFactory apiServiceFactory;
+  final endpoint = 'account/v3';
 
   AccountService(
       {required this.authenticationService, required this.apiServiceFactory});
 
   Future<List<AccountModel>> fetchAccounts() async {
-    var list = await this.apiServiceFactory.fetchList('account');
+    var list = await this.apiServiceFactory.fetchList(endpoint);
     var result = list.map((e) => AccountModel.fromJson(e)).toList();
     return result;
   }
 
   Future add(AccountModel accountModel) async {
     var body = jsonEncode(accountModel);
-    await this.apiServiceFactory.apiPost('account', body);
+    await this.apiServiceFactory.apiPost(endpoint, body);
   }
 
   Future update(AccountModel account) async {
     var msg = jsonEncode(account);
-    await this.apiServiceFactory.apiPut('account', msg);
+    await this.apiServiceFactory.apiPut(endpoint, msg);
   }
 
   Future delete(AccountModel accountModel) async {
     await this
         .apiServiceFactory
-        .apiDelete('account', accountModel.id.toString());
+        .apiDelete(endpoint, accountModel.id.toString());
   }
 }
