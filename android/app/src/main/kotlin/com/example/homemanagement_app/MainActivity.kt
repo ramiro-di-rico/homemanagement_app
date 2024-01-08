e5a95c015c72da2da960ff20bc4f4491d5976041
Ramiro Di Rico <ramiro.di.rico@pm.me>
2024-01-08 15:09:10 -0300

    Change FlutterActivity to use Flutter Fragment Activity to allow local_auth plugin to work

--- android/app/src/main/kotlin/com/example/homemanagement_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/homemanagement_app/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.homemanagement_app
 
-import io.flutter.embedding.android.FlutterActivity
-
+/*import io.flutter.embedding.android.FlutterActivity
+It fails to use local_auth plugin
 class MainActivity: FlutterActivity() {
-}
+}*/
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity : FlutterFragmentActivity()
