42aced86e57c62d6edd806882b3cf17b1a103101
Ramiro Di Rico <ramiro.di.rico@pm.me>
2024-07-29 18:00:47 -0300

    update desktop settings view

--- lib/views/main/settings_desktop.dart
+++ lib/views/main/settings_desktop.dart
@@ -6,6 +6,7 @@ import 'logging_view.dart';
 import 'settings-widgets/categories_list_widget.dart';
 import 'settings-widgets/daily-backup.widget.dart';
 import 'settings-widgets/preferred-currency-widget.dart';
+import 'settings-widgets/two-factor-authentication.widget.dart';
 
 class SettingsDesktopView extends StatefulWidget {
   const SettingsDesktopView({super.key});
@@ -47,41 +48,28 @@ class _SettingsDesktopViewState extends State<SettingsDesktopView> {
                   children: [
                     Expanded(
                       flex: 1,
-                      child: SizedBox(
-                        height: 850,
-                        child: SingleChildScrollView(
-                          child: Column(
-                            children: [
-                              DailyBackupWidget(),
-                              PreferredCurrency(),
-                              //Flexible(child: DailyBackupWdiget()),
-                              /*Padding(
-                                padding: EdgeInsets.all(10),
-                                child: TwoFactorAuthenticationWidget(),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.all(10),
-                                child: const PreferredCurreny(),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.all(10),
-                                child: _isDeveloper
-                                    ? ElevatedButton(
-                                        onPressed: () async {
-                                          await Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                              builder: (context) => LoggingView(),
-                                            ),
-                                          );
-                                        },
-                                        child: Text('Developer Mode'),
-                                      )
-                                    : Container(),
-                              ),*/
-                            ],
+                      child: Column(
+                        children: [
+                          DailyBackupWidget(),
+                          PreferredCurrency(),
+                          TwoFactorAuthenticationWidget(),
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: _isDeveloper
+                                ? ElevatedButton(
+                                    onPressed: () async {
+                                      await Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => LoggingView(),
+                                        ),
+                                      );
+                                    },
+                                    child: Text('Developer Mode'),
+                                  )
+                                : Container(),
                           ),
-                        ),
+                        ],
                       ),
                     ),
                     Expanded(
