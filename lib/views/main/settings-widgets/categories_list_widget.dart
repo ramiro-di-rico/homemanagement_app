db5e021960c7bbdfce043abf869287c6a85d7add
Ramiro Di Rico <ramiro.di.rico@pm.me>
2024-07-29 19:25:56 -0300

    Add category list header and change appearance

--- lib/views/main/settings-widgets/categories_list_widget.dart
+++ lib/views/main/settings-widgets/categories_list_widget.dart
@@ -16,57 +16,95 @@ class _CategoriesListWidgetState extends State<CategoriesListWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return RefreshIndicator(
-        child: ListView.builder(
-          shrinkWrap: true,
-          itemCount: _categoryRepository.categories.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Row(
+    return Column(
+      children: [
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            child: Row(
                 children: [
-                  Checkbox(value: true, onChanged: (value) {}),
-                  Checkbox(value: false, onChanged: (value) {}),
-                  Expanded(child: Text(_categoryRepository.categories[index].name), flex: 1,),
-                  MenuAnchor(
-                      menuChildren: [
-                        MenuItemButton(
-                          child: Text('Edit'),
-                          onPressed: () {
-                          },
-                        ),
-                        MenuItemButton(
-                          child: Text('Delete'),
-                          onPressed: () {
-                            //_categoryRepository.delete(_categoryRepository.categories[index]);
-                          },
-                        ),
-                      ],
-                    builder: (BuildContext context, MenuController controller,
-                        Widget? child) {
-                      return IconButton(
-                        onPressed: () {
-                          if (controller.isOpen) {
-                            controller.close();
-                          } else {
-                            controller.open();
-                          }
-                        },
-                        icon: const Icon(Icons.more_vert),
-                        tooltip: 'Show menu',
-                      );
+                  Text('Active', style: TextStyle(fontSize: 18)),
+                  SizedBox(width: 30),
+                  Text('Can be measured', style: TextStyle(fontSize: 18)),
+                  SizedBox(width: 30),
+                  Text('Name', style: TextStyle(fontSize: 18)),
+                  Spacer(),
+                  IconButton(
+                    onPressed: () {
+                      //Navigator.pushNamed(context, '/add-category');
                     },
+                    icon: Icon(Icons.add),
                   ),
-                ],
-              ),
-              onTap: () {
-
-              }
-            );
-          },
+                ]
+            ),
+          )
         ),
-        onRefresh: () async {
-          await _categoryRepository.load();
-        }
+        Expanded(
+          child: RefreshIndicator(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: _categoryRepository.categories.length,
+                itemBuilder: (context, index) {
+                  var category = _categoryRepository.categories[index];
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                      child: Row(
+                        children: [
+                          Checkbox(value: category.isActive, onChanged: (value) {}),
+                          SizedBox(width: 50),
+                          Checkbox(value: category.measurable, onChanged: (value) {}),
+                          SizedBox(width: 140),
+                          Expanded(
+                            child: Text(
+                                category.name,
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                            flex: 1,
+                          ),
+                          MenuAnchor(
+                              menuChildren: [
+                                MenuItemButton(
+                                  child: Text('Edit'),
+                                  onPressed: () {
+                                  },
+                                ),
+                                MenuItemButton(
+                                  child: Text('Delete'),
+                                  onPressed: () {
+                                    //_categoryRepository.delete(_categoryRepository.categories[index]);
+                                  },
+                                ),
+                              ],
+                            builder: (BuildContext context, MenuController controller,
+                                Widget? child) {
+                              return IconButton(
+                                onPressed: () {
+                                  if (controller.isOpen) {
+                                    controller.close();
+                                  } else {
+                                    controller.open();
+                                  }
+                                },
+                                icon: const Icon(Icons.more_vert),
+                                tooltip: 'Show menu',
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+              onRefresh: () async {
+                await _categoryRepository.load();
+              }
+          ),
+        ),
+      ],
     );
   }
 }
